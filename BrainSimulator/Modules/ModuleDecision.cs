10ba8416c6209538043cbd7ac8e97ee59dafaf9e
Charles Simon <charles@futureai.guru>
2024-01-16 07:21:17 -0800

    Created empty "ModuleDesicion and prototypeed dialog box.

---
+++ BrainSimulator/Modules/ModuleDecision.cs
@@ -0,0 +1,59 @@
+﻿//
+// PROPRIETARY AND CONFIDENTIAL
+// Brain Simulator 3 v.1.0
+// © 2022 FutureAI, Inc., all rights reserved
+//
+
+using System;
+using System.Collections.Generic;
+using Pluralize.NET;
+
+namespace BrainSimulator.Modules
+{
+    public class ModuleDecision : ModuleBase
+    {
+        //any public variable you create here will automatically be saved and restored  with the network
+        //unless you precede it with the [XmlIgnore] directive
+
+        public ModuleDecision()
+        {
+        }
+
+        //fill this method in with code which will execute
+        //once for each cycle of the engine
+        public override void Fire()
+        {
+            Init();  //be sure to leave this here
+
+            // if you want the dlg to update, use the following code whenever any parameter changes
+            // UpdateDialog();
+        }
+
+        // fill this method in with code which will execute once
+        // when the module is added, when "initialize" is selected from the context menu,
+        // or when the engine restart button is pressed
+        public override void Initialize()
+        {
+        }
+
+        // the following can be used to massage public data to be different in the xml file
+        // delete if not needed
+        public override void SetUpBeforeSave()
+        {
+        }
+
+        public override void SetUpAfterLoad()
+        {
+        }
+
+        // called whenever the size of the module rectangle changes
+        // for example, you may choose to reinitialize whenever size changes
+        // delete if not needed
+        public override void SizeChanged()
+        {
+            
+        }
+
+
+    }
+}
---
+++ BrainSimulator/Modules/ModuleDecision.xaml.cs
@@ -0,0 +1,89 @@
+﻿//
+// PROPRIETARY AND CONFIDENTIAL
+// Brain Simulator 3 v.1.0
+// © 2022 FutureAI, Inc., all rights reserved
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BrainSimulator.Modules
+{
+    public partial class ModuleDecisionDlg : ModuleBaseDlg
+    {
+        // Constructor of the ModuleUKSStatement dialog
+        public ModuleDecisionDlg()
+        {
+            InitializeComponent();
+        }
+
+        // Draw gets called to draw the dialog when it needs refreshing
+        public override bool Draw(bool checkDrawTimer)
+        {
+            if (!base.Draw(checkDrawTimer)) return false;
+
+            return true;
+        }
+
+        // BtnAddRelationship_Click is called the AddRelationship button is clicked
+        private void BtnAddRelationship_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // Check for thing existence and set background color of the textbox and the error message accordingly.
+        private bool CheckThingExistence(object sender)
+        {
+            if (sender is TextBox tb)
+            {
+                string text = tb.Text.Trim();
+
+                if (text == "" && !tb.Name.Contains("arget"))
+                {
+                    tb.Background = new SolidColorBrush(Colors.Pink);
+                    SetError("Source and type cannot be empty");
+                    return false;
+                }
+                List<Thing> tl = ModuleUKSStatement.ThingListFromString(text);
+                if (tl == null || tl.Count ==0)
+                {
+                    tb.Background = new SolidColorBrush(Colors.LemonChiffon);
+                    SetError("");
+                    return false;
+                }
+                tb.Background = new SolidColorBrush(Colors.White);
+                SetError("");
+                return true;
+            }
+            return false;
+        }
+
+
+        // TheGrid_SizeChanged is called when the dialog is sized
+        private void TheGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Draw(false);
+        }
+
+        // thingText_TextChanged is called when the thing textbox changes
+        private void Text_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckThingExistence(sender);
+        }
+
+        // Check for parent existence and set background color of the textbox and the error message accordingly.
+        // SetError turns the error text yellow and sets the message, or clears the color and the text.
+        private void SetError(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                errorText.Background = new SolidColorBrush(Colors.Gray);
+            else
+                errorText.Background = new SolidColorBrush(Colors.LemonChiffon);
+
+            errorText.Content = message;
+        }
+    }
+}
