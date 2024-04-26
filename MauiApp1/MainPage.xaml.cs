8e2aa22f1814df3932f9d3e8120dc209e12f8c38
Charles Simon <charles@futureai.guru>
2024-04-26 13:38:59 -0700

    Test of MAUI app

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public ObservableCollection<ModuleX> modules = new ();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            for (int i = 0; i < 10; i++)
+            {
+                modules.Add(new ModuleX(i.ToString()));
+            }
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            //if (count == 1)
+            //    CounterBtn.Text = $"Clicked {count} time";
+            //else
+            //    CounterBtn.Text = $"Clicked {count} times";
+
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+    public class ModuleX
+    {
+        public string name { get; set; }
+        public ModuleX(string name)
+        { this.name = name; }
+    }
+
+}
---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            myLabel.Text = "Fred " + count;
+            SemanticScreenReader.Announce(CounterBtn.Text);
+            myStack.Children.Add(new Label { Text = "abc" + count });
+        }
+    }
+
+}
