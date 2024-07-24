cc529320693aeae63e78d4ec79cd9605957b84c9
William Weinberger <93411345+KaiserMachine@users.noreply.github.com>
2024-07-24 16:55:16 -0700

    Added Stress Test Module
    The Stress Test Module adds numbers as strings to test the number of things that can be added at a time. Maximum is set to 10,000.

---
+++ BrainSimulator/Modules/ModuleStressTest.cs
@@ -0,0 +1,80 @@
+﻿//
+// Copyright (c) FutureAI. All rights reserved.  
+// Contains confidential and  proprietary information and programs which may not be distributed without a separate license
+//  
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using System.Windows;
+using Pluralize.NET;
+using UKS;
+using static BrainSimulator.Modules.ModuleOnlineInfo;
+
+namespace BrainSimulator.Modules
+{
+    public class ModuleStressTest : ModuleBase
+    {
+        public static string Output = "";
+
+
+        public ModuleStressTest()
+        {
+
+        }
+
+        public override void Initialize()
+        {
+
+        }
+
+        public override void SetUpAfterLoad()
+        {
+            
+        }
+
+        public static String AddManyTestItems(int count)
+        {
+            int maxCount = 10000;
+            List<string> items = new List<string>();
+
+            // Cases where we do not want to go forward.
+            if (count <= 0)
+            {
+                return "Count less or equal to 0, cannot commence.";
+            }
+            else if (count > maxCount)
+            {
+                return $"Count greater than maxCount {maxCount.ToString()}, cannot commence.";
+            }
+
+            // Add the items as strings.
+            for (int i = 0; i < count; i++)
+            {
+                items.Add(i.ToString());
+            }
+
+            // Add each item to the UKS.
+            foreach (string item in items)
+            {
+                MainWindow.theUKS.GetOrAddThing(item);
+            }
+
+            return "Items added successfully.";
+   
+        }
+
+        public override void Fire()
+        {
+            Init();  //be sure to leave this here
+
+            //if you want the dlg to update, use the following code whenever any parameter changes
+            UpdateDialog();
+        }
+
+    }
+}
---
+++ BrainSimulator/Modules/ModuleStressTestDlg.xaml.cs
@@ -0,0 +1,69 @@
+﻿//
+// Copyright (c) FutureAI. All rights reserved.  
+// Contains confidential and  proprietary information and programs which may not be distributed without a separate license
+//  
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Pluralize.NET;
+using UKS;
+
+namespace BrainSimulator.Modules
+{
+
+    public partial class ModuleStressTestDlg : ModuleBaseDlg
+    {
+
+        // UKS call
+        // Needed when accessing all items in the UKS.
+        public static ModuleHandler moduleHandler = new();
+        public static UKS.UKS theUKS = moduleHandler.theUKS;
+
+
+        public ModuleStressTestDlg()
+        {
+            InitializeComponent();
+        }
+
+        public override bool Draw(bool checkDrawTimer)
+        {
+            return base.Draw(checkDrawTimer);
+        }
+
+        private void TheCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Draw(false);
+        }
+
+        private void SetOutputText(string theText)
+        {
+            txtOutput.Text = theText;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                txtOutput.Text = "";
+                int count;
+                bool isValid = int.TryParse(textInput.Text, out count);
+                if (isValid)
+                {
+                    String message = ModuleStressTest.AddManyTestItems(count);
+                    txtOutput.Text = message;
+                }
+                else
+                {
+                    txtOutput.Text = "Error! You must provide an integer to run.";
+                }
+            }
+        }
+    }
+}
