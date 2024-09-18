7284febf9f9d4054bed7b8186b9ec4976acb2e85
William Weinberger <93411345+KaiserMachine@users.noreply.github.com>
2024-09-18 11:49:11 -0700

    Started work on Chatbot Dialogue Module
    NOTE: This is just the very beginning and it's very much a work in progress. No actual chatbot functionality is added yet. as this is just the UI systems set up.

---
+++ BrainSimulator/Modules/ModuleChatbot.cs
@@ -0,0 +1,47 @@
+﻿//
+// Copyright (c) FutureAI. All rights reserved.  
+// Contains confidential and  proprietary information and programs which may not be distributed without a separate license
+//  
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using System.Windows;
+using Pluralize.NET;
+using UKS;
+
+namespace BrainSimulator.Modules
+{
+    public class ModuleChatbot : ModuleBase
+    {
+
+
+        public ModuleChatbot()
+        {
+
+        }
+
+        public override void Initialize()
+        {
+
+        }
+
+        public override void SetUpAfterLoad()
+        {
+
+        }
+
+        public override void Fire()
+        {
+            Init();  //be sure to leave this here
+
+            //if you want the dlg to update, use the following code whenever any parameter changes
+            UpdateDialog();
+        }
+
+    }
+}
---
+++ BrainSimulator/Modules/ModuleChatbotDlg.xaml.cs
@@ -0,0 +1,72 @@
+﻿//
+// Copyright (c) FutureAI. All rights reserved.  
+// Contains confidential and  proprietary information and programs which may not be distributed without a separate license
+//  
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Pluralize.NET;
+using UKS;
+
+namespace BrainSimulator.Modules
+{
+
+    public partial class ModuleChatbotDlg : ModuleBaseDlg
+    {
+
+        // UKS call
+        // Needed when accessing all items in the UKS.
+        public static ModuleHandler moduleHandler = new();
+
+        public ModuleChatbotDlg()
+        {
+            InitializeComponent();
+        }
+
+        public override bool Draw(bool checkDrawTimer)
+        {
+            return base.Draw(checkDrawTimer);
+        }
+
+        private void TheCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Draw(false);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                String input = textInput.Text;
+                if (input.Length > 0)
+                {
+                    StatusLabel.Content = "";
+                    textPrevious.Text += "\n\nUser: " + input;
+                    textInput.Text = "";
+                }
+                else
+                {
+                    StatusLabel.Content = "Error, message cannot be empty.";
+                }
+            }
+        }
+
+        private void txtOutput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TheGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Draw(false);
+        }
+
+    }
+}
